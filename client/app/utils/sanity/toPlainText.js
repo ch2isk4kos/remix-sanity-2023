2d69c3c934f92f74e00175874602287fb8e31047
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-26 12:25:02 -0400

    create directory for sanity utils && toPlainText utility

---
+++ client/app/utils/sanity/toPlainText.js
@@ -0,0 +1,22 @@
+// Plain Text Serialization
+
+export default function toPlainText(blocks = []) {
+  return (
+    blocks
+      // loop through each block
+      .map((block) => {
+        // if it's not a text block with children,
+        // return nothing
+        if (block._type !== "block" || !block.children) {
+          return "";
+        }
+        // loop through the children spans, and join the
+        // text strings
+        return block.children.map((child) => child.text).join("");
+      })
+      // join the paragraphs leaving split by two linebreaks
+      .join("\n\n")
+  );
+}
+
+// takes a Portable Text array as an argument, and returns it as paragraphs in plain text
