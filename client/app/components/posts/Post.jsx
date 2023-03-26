c89f9ec9faab667f777883f60b43893b79ea3ea0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-26 16:55:06 -0400

    comment tailwind attribute(s)

--- client/app/components/posts/Post.jsx
+++ client/app/components/posts/Post.jsx
@@ -6,27 +6,36 @@ import { projectId, dataset } from "~/lib/sanity";
 const builder = imageUrlBuilder({ projectId, dataset });
 
 export default function Post({ post }) {
-  // console.log(post);
-  const { title, mainImage, body } = post;
+  console.log(post);
+  const { title, mainImage, body, publishedAt, author } = post;
 
   return (
     // <main className="container mx-auto prose prose-lg p-4">
-    <main className="">
-      {title ? <h1>{title}</h1> : null}
-      {mainImage ? (
-        <img
-          className="float-left m-0 w-1/3 mr-4 rounded-lg"
-          src={builder
-            .image(mainImage)
-            .width(300)
-            .height(300)
-            .quality(80)
-            .url()}
-          width={300}
-          height={300}
-          alt={title}
-        />
-      ) : null}
+    <main className="Post">
+      <div className="post-title">
+        {/*  */}
+        {title ? <h1>{title}</h1> : null}
+      </div>
+      <header className="post-header-img">
+        {mainImage ? (
+          <img
+            // className="float-left m-0 w-1/3 mr-4 rounded-lg"
+            className="rounded-lg"
+            src={builder
+              .image(mainImage)
+              .width(300)
+              .height(300)
+              .quality(80)
+              .url()}
+            width={300}
+            height={300}
+            alt={title}
+          />
+        ) : null}
+      </header>
+      <div className="post-date">
+        {publishedAt ? <p>{publishedAt}</p> : null}
+      </div>
       <div className="" style={{ border: "2px solid springGreen" }}>
         {body
           ? body.map((paragraph) => (
