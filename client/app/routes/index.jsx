d601f5310beea7d5561029b1636c9a0978f1302f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-26 20:24:46 -0400

    add comment(s)

--- client/app/routes/index.jsx
+++ client/app/routes/index.jsx
@@ -2,6 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
+    // Home Page
     <main className="flex items-center justify-center min-h-screen">
       <Link to={"/posts"} className="hover:bg-blue-50">
         Enter
